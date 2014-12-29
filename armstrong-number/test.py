b1978561ec1fb5dccf468aafe27909499bf3843a
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2014-12-29 12:38:25 +0530

    Create test.py

---
+++ armstrong-number/test.py
@@ -0,0 +1,13 @@
+from program import isArmstrong
+
+# Valid conditions
+if(isArmstrong(153)):
+  return True
+if(isArmstrong(371))
+  return True
+  
+# Invalid conditions
+if(not isArmstrong(150)):
+  return True
+if(not isArmstrong(1150)):
+  return True
