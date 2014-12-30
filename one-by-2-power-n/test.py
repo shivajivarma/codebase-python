d728fedb0f832a0e7ee9d5e7c8ef02f8031e562b
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2014-12-30 15:00:16 +0530

    Update test.py

--- one-by-2-power-n/test.py
+++ one-by-2-power-n/test.py
@@ -1,5 +1,5 @@
 from program import oneBy2PowerN
 
 # Valid conditions
-(oneBy2PowerN(5) == 0.03125)
-(oneBy2PowerN(10) == 0.0009765625)
+(oneBy2PowerN(5) == '0.03125')
+(oneBy2PowerN(10) == '0.0009765625')
