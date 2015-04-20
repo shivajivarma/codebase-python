1bd0dfd0425584605b76f226117d3e633edcbc84
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2015-04-20 09:33:24 +0530

    Update test.py

--- armstrong-number/test.py
+++ armstrong-number/test.py
@@ -1,9 +1,11 @@
 from program import isArmstrong
 
 # Valid conditions
-isArmstrong(152)
+isArmstrong(153)
 isArmstrong(371)
 
 # Invalid conditions
 not isArmstrong(150)
 not isArmstrong(1150)
+True
+False
