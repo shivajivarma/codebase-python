4e9408166386dacc9dac5547b28caa7368fa7b99
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2015-04-20 10:21:32 +0530

    Update test.py

--- armstrong-number/test.py
+++ armstrong-number/test.py
@@ -1,11 +1,16 @@
-from program import isArmstrong
+#from program import isArmstrong
 
 # Valid conditions
-isArmstrong(153)
-isArmstrong(371)
+#isArmstrong(153)
+#isArmstrong(371)
 
 # Invalid conditions
-not isArmstrong(150)
-not isArmstrong(1150)
-True
-False
+#not isArmstrong(150)
+#not isArmstrong(1150)
+
+# content of test_sample.py
+def func(x):
+  return x + 1
+  
+def test_answer():
+  assert func(3) == 5
