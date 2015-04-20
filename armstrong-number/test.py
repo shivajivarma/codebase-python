c2b0d0fddd7863cfa8fc94eafe5a77809ebd10e2
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2015-04-20 10:30:26 +0530

    Update test.py

--- armstrong-number/test.py
+++ armstrong-number/test.py
@@ -1,16 +1,7 @@
-#from program import isArmstrong
+from program import isArmstrong
 
-# Valid conditions
-#isArmstrong(153)
-#isArmstrong(371)
-
-# Invalid conditions
-#not isArmstrong(150)
-#not isArmstrong(1150)
-
-# content of test_sample.py
-def func(x):
-  return x + 1
-  
 def test_answer():
-  assert func(3) == 5
+  assert isArmstrong(153)
+  assert isArmstrong(371)
+  assert not isArmstrong(150)
+  assert not isArmstrong(1150)
